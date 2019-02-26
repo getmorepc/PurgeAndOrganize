27f9c94a6f3cca725e4b856489d4f1e68b766a68
Daniel Buchanan <daniel@getmorepc.com>
2019-02-26 15:18:31 -0500

    Dis Stinks

--- PurgeAndOrganize/Class1.cs
+++ PurgeAndOrganize/Class1.cs
@@ -1,8 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace PurgeAndOrganize
 {
     public class Class1
     {
+        //Given an array of numbers, write a function that returns an array that...
+        //Has all duplicate elements removed.
+        //Is sorted from least value to greatest value.
+
+        public static double[] UniqueSort(double[] arr)
+        {
+            return arr.Distinct().OrderBy(a => a).ToArray();
+        }
     }
 }
